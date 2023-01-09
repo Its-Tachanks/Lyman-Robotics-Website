9e6e65215240e2d199006c93f7eda897020ed187
Tachanks <joshuaew06@gmail.com>
2023-01-08 21:43:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./LandingPage";
 import LinksPage from "./LinksPage";
-import MembersPage from "./MembersPage";
 import AboutPage from "./AboutPage";
 import SchedulePage from "./SchedulePage";
 
-import "./index.css"
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/links" element={<LinksPage />} />
-      <Route path="/members" element={<MembersPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/schedule" element={<SchedulePage />} />
-    </Routes>
-  </BrowserRouter>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<LandingPage />} />
+			<Route path="/links" element={<LinksPage />} />
+			<Route path="/about" element={<AboutPage />} />
+			<Route path="/schedule" element={<SchedulePage />} />
+		</Routes>
+	</BrowserRouter>
 );
 
-console.warn("Stop fucking with my website")
+console.warn("Stop fucking with my website");
